08749a0260340094e73658ddbcaa08c9caa0369a
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-02-05 17:24:19 -0800

    Added a value of items to equal 0, initialized dataStorageType, added a welcome statement to the user, added a prompt where you get asked what data storage type you want to use, added if, else if, and if statements that gives special responses depending on whether items is not equal to 2 or 1, items is equal to 1, or if items is equal to 2, and within the else statement, added a numbered list showing options that you can do with your inventory, with an enter your choice prompt.

--- COMP003A.CodingAssignment4/Program.cs
+++ COMP003A.CodingAssignment4/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace COMP003A.CodingAssignment4
+﻿// Author: Riley Benson
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Inventory management application with a minimum of 10 elements in the collection.
+
+namespace COMP003A.CodingAssignment4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int items;
+            int dataStorageType;
+
+            Console.WriteLine("Welcome to the Inventory Management System!");
+            Console.Write("Choose data storage type: ");
+            items = int.Parse(Console.ReadLine());
+            if (items != 2 && items != 1)
+            {
+                Console.WriteLine("This is not a valid input. Try again!");
+            }
+            else if (items == 1)
+            {
+                Console.WriteLine("Sorry, I didn't use arrays in this code. Please select choice 2.");
+            }
+            else
+            {
+                Console.WriteLine("Inventory Management System Menu:");
+                Console.WriteLine("Choose data storage type:");
+                Console.WriteLine("1. Add a Product");
+                Console.WriteLine("2. Update Product Quantity");
+                Console.WriteLine("3. View Inventory Summary");
+                Console.WriteLine("4. Exit");
+                Console.Write("Enter you choice: ");
+            }
         }
     }
 }
